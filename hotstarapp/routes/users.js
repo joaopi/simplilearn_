b835f0c5af7a72217f7fd7b41a90fb5face11eb5
joaopi <joaopi@users.noreply.github.com>
2021-12-05 19:33:59 +0000

    Add cloudwatch logged user metric

--- hotstarapp/routes/users.js
+++ hotstarapp/routes/users.js
@@ -1,9 +1,54 @@
 var express = require('express');
+var AWS = require('aws-sdk');
+// Set the region 
+AWS.config.update({region: 'REGION'});
+
+// Create CloudWatch service object
+var cw = new AWS.CloudWatch();
+
+// Create parameters JSON for putMetricData
+function createMetricData(value) {
+  var params = {
+    MetricData: [
+      {
+        MetricName: 'USERS_LOGGED',
+  /*      Dimensions: [
+          {
+            Name: 'UNIQUE_PAGES',
+            Value: 'URLS'
+          },
+        ],
+  */
+        Unit: 'Count',
+        Value: value
+      },
+    ],
+    Namespace: 'SITE/TRAFFIC'
+  };
+
+  return params;
+}
+
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/login', function(req, res, next) {
+    cw.putMetricData(createMetricData(1.0), function(err, data) {
+      if (err) {
+        console.log("Error", err);
+      } else {
+        console.log("Success", JSON.stringify(data));
+      }
+    });
+});
+
+router.get('/logout', function(req, res, next) {
+    cw.putMetricData(createMetricData(-1.0), function(err, data) {
+      if (err) {
+        console.log("Error", err);
+      } else {
+        console.log("Success", JSON.stringify(data));
+      }
+    });
 });
 
 module.exports = router;
